3bbfc17895103927669186893147e0208f13de47
Mohammad Aneesh <mohammadaneesh1008@gmail.com>
2026-06-30 21:10:25 +0530

    Time: 11 ms (65.55%) | Memory: 47.2 MB (32.09%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        if (p.length()>s.length()) return ans;
+        int[] pFreq = new int[26];
+        int[] sFreq = new int[26];
+        for(int i=0;i<p.length();i++){
+            pFreq[p.charAt(i)-'a']++;
+            sFreq[s.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(pFreq, sFreq)) {
+            ans.add(0);
+        }
+        for (int i=p.length();i<s.length();i++) {
+            sFreq[s.charAt(i-p.length())-'a']--;
+            sFreq[s.charAt(i)-'a']++;             
+            if (Arrays.equals(pFreq, sFreq)) {
+                ans.add(i-p.length()+1);
+            }
+        }
+        return ans;
+    }
+}
