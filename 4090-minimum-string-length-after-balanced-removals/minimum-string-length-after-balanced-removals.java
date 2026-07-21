0a474991ad985de5d329dca99c4e4970e8643b45
Mohammad Aneesh <mohammadaneesh1008@gmail.com>
2026-07-21 06:45:13 +0530

    Time: 3 ms (99.02%) | Memory: 47.4 MB (25.00%) - LeetSync

--- 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.java
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.java
@@ -1,18 +1,11 @@
 class Solution {
     public int minLengthAfterRemovals(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(!st.isEmpty() && ch=='a' && st.peek()=='b'){
-                st.pop();
-            }
-            else if(!st.isEmpty() && ch=='b' && st.peek()=='a'){
-                st.pop();
-            }
-            else{
-                st.push(ch);
-            }
+        int a=0;
+        int b=0;
+        for(char ch : s.toCharArray()){
+            if(ch=='a') a++;
+            else b++;
         }
-        return st.size();
+        return Math.abs(a-b);
     }
 }
