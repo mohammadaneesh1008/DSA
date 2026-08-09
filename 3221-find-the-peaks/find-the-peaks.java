a3fc986a867421c50a3571e24c172d4792ede9b5
Mohammad Aneesh <mohammadaneesh1008@gmail.com>
2026-08-09 18:40:25 +0530

    Time: 1 ms (99.66%) | Memory: 45.5 MB (54.59%) - LeetSync

---
+++ 3221-find-the-peaks/find-the-peaks.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<Integer> findPeaks(int[] arr) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]) ans.add(i);
+        }
+        return ans;
+    }
+}
