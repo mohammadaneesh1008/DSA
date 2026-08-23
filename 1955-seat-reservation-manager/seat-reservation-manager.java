b32db9b4cac6567d199227fae19ac74caeb85656
Mohammad Aneesh <mohammadaneesh1008@gmail.com>
2026-08-23 17:58:03 +0530

    Time: 98 ms (48.75%) | Memory: 119.9 MB (12.86%) - LeetSync

---
+++ 1955-seat-reservation-manager/seat-reservation-manager.java
@@ -0,0 +1,21 @@
+class SeatManager {
+    PriorityQueue<Integer> pq;
+    public SeatManager(int n) {
+        pq = new PriorityQueue<>();
+        for(int i=1;i<=n;i++) pq.add(i);
+    }
+    public int reserve() {
+       if(!pq.isEmpty()) return pq.remove(); 
+       return 0;
+    }
+    public void unreserve(int seatNumber) {
+       pq.add(seatNumber); 
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
