a88a99e3ef35803f0945031c16c5564ceaeefb27
Mohammad Aneesh <mohammadaneesh1008@gmail.com>
2026-09-26 00:20:45 +0530

    Time: 2 ms (62.71%) | Memory: 45.2 MB (21.53%) - LeetSync

---
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int duplicateNumbersXOR(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int ans=0;
+        for(int x : nums){
+            if(set.contains(x)) ans^=x;
+            else set.add(x);
+        }
+        return ans;
+    }
+}
